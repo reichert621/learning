84fd423a75bfa1db912e80475aa16b166422af02
Alex Reichert <reichertjalex@gmail.com>
2017-11-11 13:08:25 -0800

    Update ch2 q3 title

--- ctci/ch-2/q3.js
+++ ctci/ch-2/q3.js
@@ -1,4 +1,6 @@
 /**
+ * Chapter 2 - Question 3
+ *
  * Delete Middle Node: Implement an algorithm to delete a node in the middle
  * (i.e., any node but the first and last node, not necessarily the exact middle)
  * of a singly linked list, given only access to that node.
